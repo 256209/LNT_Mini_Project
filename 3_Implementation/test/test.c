a761ed52a3149f5a0165481ca05576678b3d3dcf
256209 <80703475+256209@users.noreply.github.com>
2021-04-14 19:00:30 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include "search.h" 
+#include<string.h>
+#include "printname_pid.h"
+#include "unity.h"
+#include "unity_internals.h"
+#include "delete.h"
+#include "diagnosis.h"
+#include "doctor.h"
+#include "log.h"
+
+//Required by unity framework
+void setUp()
+{
+
+}
+
+//Required by unity framework
+void tearDown()
+{
+
+}
+
+//test cases to check if the DELETE functions are working fine or not
+void checkdeleteoption()
+{
+    struct patient p
+    struct patient p1;
+
+    TEST_ASSERT_EQUAL(0,0);
+}
+
+//test cases to check the SEARCH FUNCTIONS
+void checksearchfunction(){
+   
+    int date[3]={222};
+    // TEST_ASSERT_EQUAL(0,diagnosis());
+    // TEST_ASSERT_EQUAL(0,doctor());
+    TEST_ASSERT_EQUAL(0,log_patient(0, date));
+}
+int main()
+{
+    UNITY_BEGIN();
+
+    //Run commands
+    // RUN_TEST(checkdeleteoption);
+    RUN_TEST(checksearchfunction);
+
+    return UNITY_END();
+}
